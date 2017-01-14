9c3a4fc8088af17e4f98389aeba69702db86a295
Chris Barton <barton.ch@husky.neu.edu>
2017-01-14 16:28:29 -0500

    Formatting changes

--- Assets/HandTracking/Scripts/JointPosition.cs
+++ Assets/HandTracking/Scripts/JointPosition.cs
@@ -2,22 +2,22 @@
 using System.Collections;
 using Windows.Kinect;
 
-public class JointPosition : MonoBehaviour 
+public class JointPosition : MonoBehaviour
 {
     public Windows.Kinect.JointType _jointType;
     public GameObject _bodySourceManager;
     private BodySourceManager _bodyManager;
     // A vector to be used to scale joint position vector.
     private Vector3 _scalar = new Vector3(10, 10, 1);
-	
+
     // Use this for initialization
-	void Start () 
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         if (_bodySourceManager == null)
         {
@@ -46,13 +46,13 @@ public class JointPosition : MonoBehaviour
 
             if (body.IsTracked)
             {
-               //this.gameObject.transform.position = new Vector3
-               // this.gameObject.transform.localPosition =  body.Joints[_jointType].Position;
+                //this.gameObject.transform.position = new Vector3
+                // this.gameObject.transform.localPosition =  body.Joints[_jointType].Position;
                 var pos = body.Joints[_jointType].Position;
                 var z = gameObject.transform.position.z;
-               this.gameObject.transform.position = Vector3.Scale(new Vector3(pos.X, pos.Y, z), _scalar);
+                this.gameObject.transform.position = Vector3.Scale(new Vector3(pos.X, pos.Y, z), _scalar);
                 break;
             }
         }
-	}
+    }
 }
