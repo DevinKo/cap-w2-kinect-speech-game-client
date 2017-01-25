311f76bfd49b6174092999cd0fe29f1efaf7d03d
Prashant <prashantsai@hotmail.com>
2017-01-25 14:53:05 -0500

    scene test: added zone allocator game object that auto attaches the sphere zone prefab to objects with tag pointing_object

---
+++ Assets/Scripts/pointingObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointingObject : MonoBehaviour {
+
+    public GameObject sphereZonePrefab;
+    public Vector3 largerBy;
+
+	// Use this for initialization
+	void Start () {
+
+        var listOfPointObjects = GameObject.FindGameObjectsWithTag("pointing_object");
+        for (int i=0; i<listOfPointObjects.Length; i++)
+        {
+            var zone = (GameObject)Instantiate(sphereZonePrefab, listOfPointObjects[i].transform.position, listOfPointObjects[i].transform.rotation);
+            zone.transform.parent = listOfPointObjects[i].transform;
+            zone.transform.localScale = listOfPointObjects[i].transform.localScale + largerBy;
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
