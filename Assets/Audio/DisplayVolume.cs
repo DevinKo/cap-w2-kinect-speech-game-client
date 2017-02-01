8285d857b5e1c60199a7d3dd1a1bd9ae81f1a006
Kevin Do <kevindo@Kevins-MacBook-Pro.local>
2017-02-01 17:01:01 -0500

    Added intensity snapshot object

---
+++ Assets/Audio/DisplayVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayVolume : MonoBehaviour {
+
+	public Text text;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		//musicValue = _mainCam.GetComponent<Camera>().audio.volume;
+		//musicValueInt = System.Convert.ToInt32(musicValue * 100);
+	}
+}
