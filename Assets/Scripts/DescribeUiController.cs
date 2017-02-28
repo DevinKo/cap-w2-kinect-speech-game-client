4176699b602237323863bb7c4f6874d97e2dd82f
kenny864 <kenny864@ccs.neu.edu>
2017-02-28 06:04:05 -0500

    Added Describe Objective, and Game Manager to demo scene.

---
+++ Assets/Scripts/DescribeUiController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.Toolbox;
+
+public class DescribeUiController : MonoBehaviour {
+    [SerializeField]
+    private DescribeTarget dt;
+    private bool isLeft;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Make sure we dont block raycasts
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        GetComponent<CanvasGroup>().interactable = false;
+
+        dt = GameObject.FindGameObjectWithTag("DescribeController").GetComponent<DescribeTarget>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = dt.getDHandPosition(isLeft);
+	}
+}
