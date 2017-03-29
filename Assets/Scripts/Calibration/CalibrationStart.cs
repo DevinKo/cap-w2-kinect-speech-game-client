b55896daf33cdf84943f33e7ffc4e6abf318cc4a
Kevin Do <do.k@husky.neu.edu>
2017-03-29 14:04:59 -0400

    Created start timer before ReachTracker.cs

--- Assets/Scripts/Calibration/CalibrationStart.cs
+++ Assets/Scripts/Calibration/CalibrationStart.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Assets.Toolbox;
+using Windows.Kinect;
 
 public class CalibrationStart : MonoBehaviour 
 {
@@ -11,15 +13,41 @@ public class CalibrationStart : MonoBehaviour
 	public float maxTime = 5.0f; //change calibration time here
 	float timeLeft;
 
+	private Toolbox _toolbox;
+
 	// Use this for initialization
 	void Start () 
 	{
-		
+		_toolbox = FindObjectOfType<Toolbox>();
+		timeLeft = maxTime;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		// Check if joint is being tracked
+		var rightJoint = _toolbox.BodySourceManager.GetJoint(JointType.HandRight);
+		var leftJoint = _toolbox.BodySourceManager.GetJoint(JointType.HandLeft);
+		if (rightJoint == null || leftJoint == null) { return; }
+
+		// Don't countdown if any of the hands aren't tracked
+		if (rightJoint.TrackingState == TrackingState.NotTracked
+			|| leftJoint.TrackingState == TrackingState.NotTracked)
+		{
+			return;
+		}
+
+		timeLeft -= Time.deltaTime;
+		timerText.text = "Time left: " + timeLeft.ToString("f0");
+
+		if (timeLeft <= 0)
+		{
+			// turn off ReachManager object and activate PointerZoneTracker
+			GetComponent<ReachTracker> ().enabled = !GetComponent<ReachTracker> ().enabled;
+			this.enabled = !this.enabled;
+		}
+
+		// Display instructions
+		instructionText.text = "  Raise your hands and get ready!";
 	}
 }
