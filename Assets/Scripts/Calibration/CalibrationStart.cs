a9dddecf3173d3b700e94fc2f6040697f23e53ea
Kevin Do <do.k@husky.neu.edu>
2017-03-29 13:41:19 -0400

    add/set CalibrationStart.cs

---
+++ Assets/Scripts/Calibration/CalibrationStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalibrationStart : MonoBehaviour 
+{
+	public Text testText; // Used for displaying audio threshold (testing only)
+	public Text timerText;
+	public Text instructionText;
+	public float maxTime = 5.0f; //change calibration time here
+	float timeLeft;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
