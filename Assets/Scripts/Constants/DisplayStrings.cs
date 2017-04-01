b656f4ee1c84b7e8bfded79e7f4dc6494de82671
Prashant <prashantsai@hotmail.com>
2017-04-01 11:59:59 -0400

    added more prompts; prompts automatically transition during the describe scene; added level completion text; moved some ui elements

--- Assets/Scripts/Constants/DisplayStrings.cs
+++ Assets/Scripts/Constants/DisplayStrings.cs
@@ -7,7 +7,10 @@ namespace Assets.Scripts.Constants
 {
     public static class DisplayStrings
     {
-        public static string Instruction_FindClue = "Find the clue, by pointing at it!";
-        public static string Instruction_DescribeClue = "How big is it?\nDescribe the clue, by putting your hands around the object!";
+        public static string Instruction_FindClue = "Find the clue, by pointing at it with both hands!";
+        public static string Instruction_DescribeClue = "How big is it?\nDescribe the clue, by putting your hands around the object!\n*Hint: keep the distance between you hands wider than the clue";
+        public static string Instruction2_DescribeClue = "While you are doing this, tell me how big the clue is!\nIs it large or small or somewhere in between?";
+        public static string Instruction3_DescribeClue = "Is it round? What colors does it have?\nTell me more about the object!";
+        public static string Completion_Text = "Awesome job! Now that we have anough information for this clue, let's\nmove on to the next one!";
     }
 }
